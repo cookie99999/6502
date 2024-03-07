c968af94ad4b4fdce1f90c739defb53d21d9e084
cookie99999 <77994115+cookie99999@users.noreply.github.com>
2024-03-07 01:46:39 -0500

    work on macros

--- forthtest.asm
+++ forthtest.asm
@@ -1,5 +1,46 @@
 	.segment "CODE"
 
+	IP = $0002 		; 2 bytes
+	SCRATCH = $0006 	;2 bytes
+
+	.macro INC16 addr
+	inc addr
+	bne @skip
+	inc addr + 1
+@skip:
+	.endmacro
+
+	.macro NEXT
+	ldy #1
+	lda (IP), y
+	sta SCRATCH + 1
+	dey
+	lda (IP), y
+	sta SCRATCH
+	clc
+	lda IP
+	adc #2
+	sta IP
+	bcc @skip
+	inc IP + 1
+@skip:
+	jmp SCRATCH - 1		;JMP opcode at SCRATCH - 1
+	.endmacro
+
+	.macro PUSHRSP
+	lda IP+1
+	pha
+	lda IP
+	pha
+	.endmacro
+
+	.macro POPRSP
+	pla
+	sta IP
+	pla
+	sta IP+1
+	.endmacro
+
 	link .set 0
 	
 	.macro defword name, namelen, flags, label
@@ -32,12 +73,21 @@
 	.ident(.sprintf("code_%s", label)):
 .endmacro
 
-	defcode "DROP", 4, , DROP
+	defcode "DROP", 4, , DROP ; ( -- )
 	inx
 	inx
 	NEXT
 
-	defcode "SWAP", 4, , SWAP
+	defcode "DUP", 3, , DUP	; ( a -- a a )
+	dex
+	dex
+	lda 2, x
+	sta 0, x
+	lda 3, x
+	sta 1, x
+	NEXT
+
+	defcode "SWAP", 4, , SWAP ; ( a b -- b a )
 	lda 0, x
 	ldy 2, x
 	sta 2, x
@@ -47,6 +97,19 @@
 	sta 3, x
 	sty 1, x
 	NEXT
+
+	defcode "+", 1, , PLUS 	; ( a b -- a+b)
+	clc
+	lda 0, x
+	adc 2, x 		; low bytes added
+	sta 2, x
+	lda 1, x
+	adc 3, x
+	sta 3, x
+	inx
+	inx 			; DROP
+	NEXT
+	
 DOCOL:
 	.byte $55, $aa
 
