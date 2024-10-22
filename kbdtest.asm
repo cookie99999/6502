dec7317dd73651a44c91d38369e088881c059ffd
cookie99999 <77994115+cookie99999@users.noreply.github.com>
2024-10-21 22:03:05 -0400

    wozmon and basic attempts

--- kbdtest.asm
+++ kbdtest.asm
@@ -1,103 +1,151 @@
-	.include "hardware.inc"
-		VIA_IFR_CA1 = $02
+  .setcpu "65C02"
+  .include "hardware.inc"
+  VIA_IFR_CA1 = $02
 
-	.segment "CODE"
-	tv_idle = $2000 	; 1 byte
+  .segment "CODE"
+  tv_idle = $2000 	; 1 byte
+  work = $2001
+  inbuf = $0200
 
-	jmp init
+  jmp main
 
-	.include "protoforth.asm"
+  .include "protoforth.asm"
 teststr:
-	.byte "peepee poopoo", 0
-intstr:
-	.byte "int ", 0
-
-init:
-  sei
-	ldx #$ff
-	txs
-
-	lda VIA2_DDRB
-	and #$7f
-	sta VIA2_DDRB 		; bit 7 input
-	lda VIA2_ACR
-	and #$e3
-	ora #$18
-	sta VIA2_ACR		; shift out under control of phi2
-	lda #$04
-	sta VIA2_IER		; disable shift register interrupts
-
-  stz VIA2_DDRA ; all inputs
-  lda VIA2_PCR
-  ;ora #$0a
-  ;and #$fa ; ca2 pulse output, ca1 negative edge
-  ora #$0e
-  and #$fe ;ca2 pulse ca1 high
-  sta VIA2_PCR
-
-  lda #$82 ;set mode, set ca1
-  sta VIA2_IER
-	stz tv_idle
+  .byte "peepee poopoo", 0
 
 main:
-	PUT $200
-	jsr DELAY 		; wait for avr to warm up
-	lda #$0c		; form feed
-	jsr tv_output
-	lda #'X'
-	jsr tv_output
-	PUT teststr
-	jsr tv_puts
+  jsr init
+  ldx #$00
 loop:
+  jsr getchar
+  cmp #$0d ; cr
+  bne loopskip0
+  jsr putline
+  ldx #$00
+  bra loop
+loopskip0:
+  sta inbuf, x
+  jsr tv_output
+  inx
+  jmp loop
+
+putline:
+  lda #$0d
+  jsr tv_output
+  phx
+  phy
+  ldy #$00
+plloop0:
+  lda inbuf, y
+  jsr tv_output
+  iny
+  sty work
+  cpx work
+  beq plend
+  ldy work
+  bra plloop0
+plend:
+  lda #$0d
+  jsr tv_output
+  ply
+  plx
+  rts
+
+getchar:
   lda VIA2_IFR
   and #$02
-  beq loop
-  PUT intstr
-  jsr tv_puts
+  beq getchar
+  lda #$02
+  sta VIA2_IFR
   lda VIA2_PA
-  pha
-  lda VIA2_PCR
-  ora #$0c
-  and #$fc
-  sta VIA2_PCR ; pulse data ack line
-  PUT $80
-  jsr DELAY
-  ora #$0e
-  and #$fe
-  sta VIA2_PCR
-  pla
-  beq loop
-  jsr tv_output
-	jmp loop
+  beq getchar
+  rts
 
 tv_output:
-	pha
-	lda tv_idle
-	eor #$80
-	sta tv_idle
+  pha
+  lda tv_idle
+  eor #$80
+  sta tv_idle
 @1:
-	lda VIA2_PB
-	eor tv_idle
-	bpl @1 			; wait until avr is idle and ready
-	pla
-	sta VIA2_SR		; shift out command/data
-	rts
+  lda VIA2_PB
+  eor tv_idle
+  bpl @1 			; wait until avr is idle and ready
+  pla
+  sta VIA2_SR		; shift out command/data
+  rts
 
 tv_output2:
-	bit VIA2_PB
-	bmi tv_output2 ; wait for ready
-	sta VIA2_SR
-	rts
+  bit VIA2_PB
+  bmi tv_output2 ; wait for ready
+  sta VIA2_SR
+  rts
 
 tv_puts:
-	lda (0, x)
-	beq @quit		; zero terminator
-	jsr tv_output
-	jsr INC_16
-	jmp tv_puts
+  lda (0, x)
+  beq @quit		; zero terminator
+  jsr tv_output
+  jsr INC_16
+  jmp tv_puts
 @quit:
-	DROP
-	rts
+  DROP
+  rts
 
-	.segment "RESETVEC"
-	.word $0000, init, $0000
+init:
+  ; port b (tv text output avr)
+  lda VIA2_DDRB
+  and #$7f ; bit 7 is input
+  sta VIA2_DDRB
+  ;lda VIA2_ACR
+  ;and #$e3
+  ;ora #$18
+  ;sta VIA2_ACR ; shift out under control of phi2
+  lda #$1b
+  sta VIA2_ACR
+  lda #$04
+  sta VIA2_IER ; disable shift register interrupts
+
+  ; port a (avr keyboard controller)
+  stz VIA2_DDRA ; all inputs
+  lda VIA2_PCR
+  ora #$0a
+  and #$fa
+  sta VIA2_PCR ; ca2 pulse output, ca1 negative edge
+  ;lda VIA2_ACR
+  ;ora #1
+  ;sta VIA2_ACR ; enable latching on port a
+  lda #$82
+  sta VIA2_IER ; enable ca1 interrupts
+
+  lda #$00
+  sta tv_idle
+  PUT $2000
+  jsr DELAY
+  lda #$0c
+  jsr tv_output ; form feed to clear screen
+  ldy #$7f
+  lda #%10100111
+
+  lda #$aa
+  sta $0200
+  lda #$55
+  sta $0201
+  lda $0200
+  cmp #$aa
+  bne erri
+  lda $0201
+  cmp #$55
+  bne errii
+  lda #'O'
+  jsr tv_output
+  rts
+erri:
+  lda #'X'
+  jsr tv_output
+  rts
+errii:
+  lda #'Y'
+  jsr tv_output
+  rts
+
+  .segment "RESETVEC"
+  .word $0000, main, $0000
